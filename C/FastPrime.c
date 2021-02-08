b662f6f57ad0cd6ebd1bde0588e91327f45c3a35
Daniel de Souza Lima <39025412+OHomemParede@users.noreply.github.com>
2021-02-08 02:44:44 -0300

    Create FastPrime.c

---
+++ C/FastPrime.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+int isprime(int n);
+int isprime(int n){
+  unsigned int cont;
+  if(n==2) return 1;
+  if(n%2==0) return 0;
+  for(cont=3;cont<=pow(n,0.5);cont=cont+2){
+    if(n%cont==0){
+      return 0;
+    }
+  }
+  return 1;
+}
+
+int main() {
+  int n,a, cont;
+  scanf("%d", &a);
+  for(cont=0;cont<a;cont++){
+    scanf("%d", &n);
+    
+    if(!isprime(n))
+      printf("Not Prime\n");
+  
+    else
+      printf("Prime\n");
+  
+  }
+}
